db0d8edf991404189dd908f330cd6283695a7b01
Thiaguito2024 <thiagojlgd@gmail.com>
2025-05-29 21:56:09 -0300

    Subiendo tp integrador interfaces

--- Guia Herencia/TrabajoIntegradorHerencia/src/baraja/CartaComodin.java
+++ Guia Herencia/TrabajoIntegradorHerencia/src/baraja/CartaComodin.java
@@ -3,6 +3,6 @@ package baraja;
 public class CartaComodin extends Carta{
     
     public CartaComodin(String nombre) {
-        super("Comoding", nombre);
+        super("Comodin", nombre);
     }
 }
